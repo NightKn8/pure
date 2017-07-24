6d4b6182340297d2203fe9e6e6fe7ba9335fa3a6
NightKn8 <k.jedraszewski@gmail.com>
2017-07-24 18:53:15 +0200

    d

---
+++ js/demo3.js
@@ -0,0 +1,61 @@
+class temperatureWidget {
+  constructor(parentDOM) {
+    this.parentDOM = document.getElementById(parentDOM);
+    this.retryTimer;
+    this.updateTimer;
+    this.retry = 0;
+  }
+  request() {
+    (this.retry <= 2) ? this.parentDOM.innerHTML = `<section id="tempWidget">LOADING<i class="fa fa-spinner fa-pulse fa-3x"></i></section>`: this.parentDOM.innerHTML = `<section id="tempWidget">LOADING... SERVER WHERE ARE YOU?<i class="fa fa-spinner fa-pulse fa-3x"></i></section>`;
+    const request = $.ajax({
+      type: 'GET',
+      url: "http://nightkn8.dlinkddns.com",
+      dataType: "json",
+      success: (data) => data
+    });
+    request.done((obj) => {
+      clearTimeout(this.retryTimer);
+      clearTimeout(this.updateTimer);
+      this.temp = obj.temperature;
+      this.date = obj.date;
+      this.time = () => {
+        const h = obj.time.split(':')[0],
+          m = obj.time.split(':')[1];
+        return `${h}:${m}`;
+      }
+      this.retry = 0;
+      this.render();
+      this.updateTimer = setTimeout(() => {
+        this.request();
+      }, 300000)
+    }).fail(this.retryTimer = setTimeout(() => {
+      clearTimeout(this.retryTimer);
+      clearTimeout(this.updateTimer);
+      this.retry++;
+      this.request();
+    }, 10000));
+  };
+  render() {
+    return this.parentDOM.innerHTML = `
+        <section id="tempWidget">
+            <section id="temp">
+                ${Math.round(this.temp)}&#176;
+            </section>
+            <section id="location">
+                <i class="fa fa-map-marker" aria-hidden="true"></i> DOBROWODA
+            </section>
+            <section id="date">
+                <section id="time">
+                    ${this.time()}
+                </section>
+                <section id="day">
+                    ${new Date(this.date).toLocaleDateString()}
+                </section>
+            </section>
+        </section>`;
+  }
+};
+const tempWidget = new temperatureWidget("container");
+$(document).ready(() => {
+  tempWidget.request();
+});
